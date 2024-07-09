c2a5b95be0d6d2f5db05924c40acdf5c2a3d33f7
TheWizardGuy333 <sharethegoldenwealthnet@gmail.com>
2024-07-09 11:10:35 -0700

    Create config.py
    config

---
+++ Marketplace (app/app/app/app/config.py
@@ -0,0 +1,6 @@
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    STRIPE_SECRET_KEY = 'your_stripe_secret_key'
+    STRIPE_PUBLISHABLE_KEY = 'your_stripe_publishable_key'
+    DEEPAI_API_KEY = 'your_deepai_api_key'
